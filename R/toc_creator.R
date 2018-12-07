fc82dd6459a5043bd8a0ae287c0ae0b7a8409c18
takewiki <hulilei@takewiki.com.cn>
2018-12-07 23:27:21 +0800

    initial setup

---
+++ R/toc_creator.R
@@ -0,0 +1,16 @@
+#' 在当前目录下创建目录
+#'
+#' @param path_Names  字符串，向量
+#'
+#' @return 返回true
+#' @export
+#'
+#' @examples toc_creator(letters)
+toc_creater <- function (path_Names,includeData=TRUE){
+  if ( includeData == TRUE){
+    add_data <- c('srcData','testData','demoData')
+    path_Names <- c(path_Names,add_data)
+  }
+  lapply(path_Names,dir.create)
+  return(TRUE)
+}
